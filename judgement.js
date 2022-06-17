101ecbbc6143fecf4f3dcf3784429b4690327882
ゆききつね <107163439+Yukiika@users.noreply.github.com>
2022-06-17 21:28:00 +0900

    JankenGame
    1st commit

---
+++ judgement.js
@@ -0,0 +1,15 @@
+﻿function myResult(jankenUser){
+	var jankenComp = Math.floor(Math.random()*3);
+	var jankenName = ["グー", "チョキ", "パー"];
+	var strMsg1 = "あいこです";
+	var strMsg2 = "あなたの勝ちです";
+	var strMsg3 = "あなたの負けです";
+	var strMsg = "相手は" + jankenName[jankenComp] + "、";
+	if(jankenComp == jankenUser) {
+		return strMsg + strMsg1;
+	} else if((jankenComp+2)%3 == jankenUser) {
+		return strMsg + strMsg2;
+	} else {
+		return strMsg + strMsg3;
+	}
+}
